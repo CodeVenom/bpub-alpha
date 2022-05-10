2457ea44162b1e8ce6235bc7a4f20cb555f7ca38
CodeVenom <juri.jaeger@gmail.com>
2022-05-10 23:39:01 +0200

    fix html content type in bucket

--- .github/workflows/pages.sh
+++ .github/workflows/pages.sh
@@ -16,8 +16,8 @@ create() {
 
     # TODO: check if html is already there => html has precedence, otherwise convert md to html
     pandoc -f markdown_strict -t html -o "pages/$page/$page.html" "pages/$page/$page.md"
-    sed -i -e "/<jj-content><\/jj-content>/r pages/$page/$page.html" "temp/$page.html"
-    sed -i -e "/<jj-content><\/jj-content>/d" "temp/$page.html"
+    sed -i -e "/<jj-content><\/jj-content>/r pages/$page/$page.html" "temp/$page"
+    sed -i -e "/<jj-content><\/jj-content>/d" "temp/$page"
 
     cp -R "pages/$page/assets/" "temp/assets/"
   done
