9d0f74e3e3b5510c79dd5ebad298b6e199bce333
CodeVenom <juri.jaeger@gmail.com>
2022-05-11 17:43:34 +0200

    pre-bake pages

--- .github/workflows/pages.sh
+++ .github/workflows/pages.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 create() {
-  # TODO: pre-bake page without content
+  local pageTemplate="templates/page.html"
+  local prebakedPage="templates/prebaked.html"
   local snippets="meta style"
   local pages
   pages=$(ls pages/)
 
+  cp "$pageTemplate" "$prebakedPage"
+
+  for snippet in $snippets; do
+    sed -i -e "/<jj-$snippet><\/jj-$snippet>/r snippets/$snippet.html" "$prebakedPage"
+    sed -i -e "/<jj-$snippet><\/jj-$snippet>/d" "$prebakedPage"
+  done
+
   for page in $pages; do
-    cp templates/page.html "temp/$page"
-
-    for snippet in $snippets; do
-      sed -i -e "/<jj-$snippet><\/jj-$snippet>/r snippets/$snippet.html" "temp/$page"
-      sed -i -e "/<jj-$snippet><\/jj-$snippet>/d" "temp/$page"
-    done
-
     # TODO: check if html is already there => html has precedence, otherwise convert md to html
+    # TODO: update title here
+    cp "$prebakedPage" "temp/$page"
     pandoc -f markdown_strict -t html -o "pages/$page/$page.html" "pages/$page/$page.md"
     sed -i -e "/<jj-content><\/jj-content>/r pages/$page/$page.html" "temp/$page"
     sed -i -e "/<jj-content><\/jj-content>/d" "temp/$page"
